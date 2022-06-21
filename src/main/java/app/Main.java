ce217840e285962e998b22f4a00148b73009193f
robi.1993 <robertsafetovic@hotmail.com>
2022-06-21 16:41:39 +0200

    book controller

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,20 @@
+package app;
+
+import app.book.BookController;
+import app.book.BookDao;
+import io.javalin.Javalin;
+
+public class Main {
+    public static void main(String[] args) {
+
+           BookDao bookDao = new BookDao();
+
+            Javalin app = Javalin.create().start(7001);
+            app.get("/books", BookController::fetchAllBooks);
+
+
+
+
+
+        }
+}
--- src/main/java/app/book/BookController.java
+++ src/main/java/app/book/BookController.java
@@ -1,7 +1,27 @@
 package app.book;
 
+import io.javalin.http.Context;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Handler;
 
-public class BookController {
 
-}
+    public class BookController {
+
+        static BookDao bookDao = new BookDao();
+        public static void fetchAllBooks (Context ctx) {
+            Map<String, Object> model = new HashMap<>();
+            model.put("books", bookDao.getAllBooks());
+            ctx.json(model);
+        };
+
+        public static void fetchOneBook (Context ctx ) {
+            Map<String, Object> model = new HashMap<>();
+            String isbn = ctx.pathParam("isbn");
+            model.put("app/book", bookDao.getBookByIsbn(isbn));
+            ctx.json(model);
+        };
+    }
+
+
